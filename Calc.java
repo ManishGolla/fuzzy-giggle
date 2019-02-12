5b9a9cada8dd02a2f22f3987e9b44b423b99a3ae
Golla <724431@cognizant.com>
2019-02-12 17:58:01 +0530

    java file modified

--- Calc.java
+++ Calc.java
@@ -11,7 +11,9 @@ public class Calc{
             System.out.print("2. Subtraction \n");
             System.out.print("3. Multiplication\n");
             System.out.print("4. Division\n");
-            System.out.print("5. Exit\n\n");
+            System.out.print("5. Power of 2\n");
+            System.out.print("6. Squareroot\n");
+            System.out.print("7. Exit\n\n");
             System.out.print("Enter Your Choice : ");
             choice = scan.next().charAt(0);
             switch(choice)
@@ -21,6 +23,9 @@ public class Calc{
                 case '3' :
                 case '4' :
                 case '5' :
+                case '6' :
+                case '7' : System.exit(0);
+                    break;
                 default : System.out.print("Wrong Choice!!!");
                     break;
             }
