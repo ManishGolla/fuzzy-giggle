6953d71009385f7be904a021301a824e2307b270
Golla <724431@cognizant.com>
2019-02-12 17:47:12 +0530

    java file added

---
+++ Calc.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Calc{
+    public static void main(String[] args){
+        float a, b, res;
+        char choice, ch;
+         do
+        {
+            System.out.print("1. Addition \n");
+            System.out.print("2. Subtraction \n");
+            System.out.print("3. Multiplication\n");
+            System.out.print("4. Division\n");
+            System.out.print("5. Exit\n\n");
+            System.out.print("Enter Your Choice : ");
+            choice = scan.next().charAt(0);
+            switch(choice)
+            {
+                case '1' :
+                case '2' : 
+                case '3' :
+                case '4' :
+                case '5' :
+                default : System.out.print("Wrong Choice!!!");
+                    break;
+            }
+            System.out.print("\n---------------------------------------\n");
+        }while(choice != 5); 
+    }
+}
